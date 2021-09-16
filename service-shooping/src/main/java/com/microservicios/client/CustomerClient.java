33605d0986abafe245128ddfbdfa5119d341c8f6
Alejandro <alejo_540@hotmail.com>
2021-09-16 16:01:22 -0500

    Commit

--- service-shooping/src/main/java/com/microservicios/client/CustomerClient.java
+++ service-shooping/src/main/java/com/microservicios/client/CustomerClient.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservicios.model.Customer;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
 
+  @HystrixCommand
   @GetMapping(value = "/customers/{id}")
   public ResponseEntity<Customer> getCustomer(@PathVariable("id") Long id);
   
